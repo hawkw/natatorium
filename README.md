50af356f291ebb5ee133dbc6df5664e5b99787ce
Eliza Weisman <eliza@buoyant.io>
2019-08-12 12:10:47 -0700

    add comparison, more benches
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- README.md
+++ README.md
@@ -22,10 +22,7 @@ object pooling, and there's already a crate called `pool`.
     `growable::Pool` type which grows when at capacity.
   - In both `pool` and `natatorium`, releasing an object back to the pool is
     lock-free. However, `pool` requires a mutable reference to check an object
-    _out_ of the pool, while `natatorium` does not. `natatorium`'s `fixed::Pool`
-    type allows lock-free checkouts, while its `growable::Pool` type requires a
-    read lock for checking out objects when the pool has capacity, and a write
-    lock for checking out objects when growing the pool.
+    _out_ of the pool, while `natatorium` does not.
   - `pool` provides access to a fixed-size array of bytes for every pooled
     object, which the documentation suggests using for storing user-defined
     metadata associated with that object. `natatorium` does not support this,
@@ -35,6 +32,36 @@ object pooling, and there's already a crate called `pool`.
     `natatorium` has `Owned` checkout types with similar guarantees, but allows
     downgrading an `Owned` checkout to a reference-counted `Shared` checkout,
     which can be cloned and allows only immutable access.
+  - Because `pool` provides fewer features than `natatorium`, its implementation
+    is somewhat simpler, and checkout performance is faster in our benchmarks
+    (by a constant factor of about 15-20 nanoseconds).
+
   <!-- while `natatorium` always expects them to live on the heap --- `natatorium` is
   intended primarily for managing the reuse of growable objects like strings,
   buffers, and maps. -->
+  [`pool`]: https://crates.io/crates/pool
+- [`object-pool`]: The `object-pool` crate provides a simple lock-free object
+  pool for typed objects.
+
+  - Like `natatorium`, both checking out and checking in an object are
+    lock-free, so a global lock is not required when sharing `object-pool`'s
+    pool across multiple threads.
+  - `object-pool`'s pool is fixed size, and cannot be increased in size once it
+    has been constructed.
+  - `object-pool` does not support downgrading an owned checkout into a shared
+    checkout. Instead, all objects checked out from the pool are uniquely owned.
+  - `object-pool` does not automatically reset checkouts on drop. Instead, the
+    user [is responsible] for manually clearing checked out objects.
+  - `object-pool`'s checkout algorithm pool has a worst-case time complexity of
+    O(_m_), where _m_ is the size of the pool. Performance approaches the worst
+    case as utilization increase (i.e. as more objects are checked out) &mdash
+    when the final free object is checked out, `object-pool`'s checkout
+    algorithm must iterate over every other object in the pool before finding
+    the final block. In comparison, `natatorium::fixed`'s worst-case time
+    complexity for checkouts is O(_1_), while `natatorium::growable` is
+    O(log<sub>2</sub> _m_) in the worst case, and amortized as utilization
+    increases. In our benchmarks, this property can be observed to hurt
+    `object-pool` checkout performance significantly as utilization increases.
+
+    [is responsible]: https://docs.rs/object-pool/0.3.1/object_pool/index.html#warning
+    [`object-pool`]: https://crates.io/crates/object-pool
--- benches/checkout.rs
+++ benches/checkout.rs
@@ -4,17 +4,21 @@ use criterion::{criterion_group, criterion_main};
 use criterion::black_box;
 use natatorium::{growable, fixed};
 use pool;
+use object_pool;
 
 static KB: usize = 1024;
 static MB: usize = 1024 * KB;
 static GB: usize = 1024 * MB;
-static SIZES: &[usize] = &[
+static VEC_SIZES: &[usize] = &[
     4 * KB, 16 * KB, 64 * KB, 128 * KB, 512 * KB, 1 * MB, 16 * MB, 32 * MB, 64 * MB, 128 * MB,
-    256 * MB, 512 * MB, 1 * GB, // 2 * GB, 3 * GB,
+    256 * MB, 512 * MB, 1 * GB, 2 * GB, 3 * GB,
 ];
 
+static POOL_SIZES: &[usize] = &[
+    4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048,
+];
 
-fn checkout(c: &mut Criterion) {
+fn checkout_once(c: &mut Criterion) {
     let bench = criterion::ParameterizedBenchmark::new("natatorium::growable", |b, &&i| {
             let pool = growable::Pool::builder()
                 .with_elements(1)
@@ -22,18 +26,13 @@ fn checkout(c: &mut Criterion) {
             b.iter(|| {
                 black_box(pool.checkout());
             })
-    }, SIZES)
+    }, VEC_SIZES)
         .with_function("natatorium::fixed", |b, &&i| {
-            let pool = growable::Pool::builder()
+            let pool = fixed::Pool::builder()
                 .with_elements(1)
                 .with_fn(|| Vec::<u8>::with_capacity(i)).finish();
             b.iter(|| {
                 black_box(pool.checkout());
-            })
-        })
-        .with_function("alloc", |b, &&i| {
-            b.iter(|| {
-                black_box(Vec::<u8>::with_capacity(i));
             })
         })
         .with_function("pool", |b, &&i| {
@@ -41,40 +40,167 @@ fn checkout(c: &mut Criterion) {
             b.iter(|| {
                 black_box(pool.checkout().unwrap());
             })
+        })
+        .with_function("object_pool", |b, &&i| {
+            let pool = object_pool::Pool::new(1, || Vec::<u8>::with_capacity(i));
+            b.iter(|| {
+                black_box(pool.pull().unwrap());
+            })
+        })
+        .with_function("alloc", |b, &&i| {
+            b.iter(|| {
+                black_box(Vec::<u8>::with_capacity(i));
+            })
         });
-    c.bench("checkout_one", bench);
+    c.bench("checkout_once", bench);
 }
 
-// fn checkout_checkin(c: &mut Criterion) {
-//     let bench = criterion::ParameterizedBenchmark::new("natatorium::growable", |b, &&i| {
-//             let pool = growable::Pool::builder()
-//                 .with_elements(1)
-//                 .with_fn(|| Vec::<u8>::with_capacity(i)).finish();
-//             b.iter(|| {
-//                 let ch = pool.checkout();
-//                 black_box(&ch);
-//                 drop(ch);
-//             })
-//     }, SIZES)
-//         .with_function("natatorium::fixed", |b, &&i| {
-//             let pool = growable::Pool::builder()
-//                 .with_elements(1)
-//                 .with_fn(|| Vec::<u8>::with_capacity(i)).finish();
-//             b.iter(|| {
-//                 let ch = pool.checkout();
-//                 black_box(&ch);
-//                 drop(ch);
-//             })
-//         })
-//         .with_function("alloc", |b, &&i| {
-//             b.iter(|| {
-//                 let ch = Vec::<u8>::with_capacity(i);
-//                 black_box(&ch);
-//                 drop(ch);
-//             })
-//         });
-//     c.bench("checkout_one", bench);
-// }
+fn checkout_twice(c: &mut Criterion) {
+    let bench = criterion::ParameterizedBenchmark::new("natatorium::growable", |b, &&i| {
+            let pool = growable::Pool::builder()
+                .with_elements(1)
+                .with_fn(|| Vec::<u8>::with_capacity(i)).finish();
+            b.iter(|| {
+                black_box(pool.checkout());
+                black_box(pool.checkout());
+            })
+    }, VEC_SIZES)
+        .with_function("natatorium::fixed", |b, &&i| {
+            let pool = fixed::Pool::builder()
+                .with_elements(1)
+                .with_fn(|| Vec::<u8>::with_capacity(i)).finish();
+            b.iter(|| {
+                black_box(pool.checkout());
+                black_box(pool.checkout());
+            })
+        })
+        .with_function("pool", |b, &&i| {
+            let mut pool = pool::Pool::with_capacity(1, 0, || Vec::<u8>::with_capacity(i));
+            b.iter(|| {
+                black_box(pool.checkout().unwrap());
+                black_box(pool.checkout().unwrap());
+            })
+        })
+        .with_function("object_pool", |b, &&i| {
+            let pool = object_pool::Pool::new(1, || Vec::<u8>::with_capacity(i));
+            b.iter(|| {
+                black_box(pool.pull().unwrap());
+            })
+        })
+        .with_function("alloc", |b, &&i| {
+            b.iter(|| {
+                black_box(Vec::<u8>::with_capacity(i));
+                black_box(Vec::<u8>::with_capacity(i));
+            })
+        });
+    c.bench("checkout_twice", bench);
+}
 
-criterion_group!(benches, checkout);
+fn checkout_last(c: &mut Criterion) {
+    let bench = criterion::ParameterizedBenchmark::new("natatorium::growable", |b, &&i| {
+            let pool = growable::Pool::builder()
+                .with_elements(i)
+                .with_fn(|| Vec::<u8>::new()).finish();
+            let mut checkouts = Vec::new();
+            for _ in 0..(i - 1) {
+                checkouts.push(pool.checkout());
+            }
+            b.iter(|| {
+                black_box(pool.checkout());
+            });
+            black_box(checkouts);
+    }, POOL_SIZES)
+        .with_function("natatorium::fixed", |b, &&i| {
+            let pool = fixed::Pool::builder()
+                .with_elements(i)
+                .with_fn(|| Vec::<u8>::new()).finish();
+            let mut checkouts = Vec::new();
+            for _ in 0..(i - 1) {
+                checkouts.push(pool.checkout());
+            }
+            b.iter(|| {
+                black_box(pool.checkout());
+            });
+            black_box(checkouts);
+        })
+        .with_function("pool", |b, &&i| {
+            let mut pool = pool::Pool::with_capacity(i, 0, || Vec::<u8>::new());
+            let mut checkouts = Vec::new();
+            for _ in 0..(i - 1) {
+                checkouts.push(pool.checkout().unwrap());
+            }
+            b.iter(|| {
+                black_box(pool.checkout().unwrap());
+            });
+            black_box(checkouts);
+        })
+        .with_function("object_pool", |b, &&i| {
+            let pool = object_pool::Pool::new(i, || Vec::<u8>::new());
+            let mut checkouts = Vec::new();
+            for _ in 0..(i - 1) {
+                checkouts.push(pool.pull().unwrap());
+            }
+            b.iter(|| {
+                black_box(pool.pull().unwrap());
+            });
+            black_box(checkouts);
+        });
+    c.bench("checkout_last", bench);
+}
+
+
+fn checkout_third(c: &mut Criterion) {
+    let bench = criterion::ParameterizedBenchmark::new("natatorium::growable", |b, &&i| {
+            let pool = growable::Pool::builder()
+                .with_elements(i)
+                .with_fn(|| Vec::<u8>::new()).finish();
+            let mut checkouts = Vec::new();
+            for _ in 0..(i/3) {
+                checkouts.push(pool.checkout());
+            }
+            b.iter(|| {
+                black_box(pool.checkout());
+            });
+            black_box(checkouts);
+    }, POOL_SIZES)
+        .with_function("natatorium::fixed", |b, &&i| {
+            let pool = fixed::Pool::builder()
+                .with_elements(i)
+                .with_fn(|| Vec::<u8>::new()).finish();
+            let mut checkouts = Vec::new();
+            for _ in 0..(i/3) {
+                checkouts.push(pool.checkout());
+            }
+            b.iter(|| {
+                black_box(pool.checkout());
+            });
+            black_box(checkouts);
+        })
+        .with_function("pool", |b, &&i| {
+            let mut pool = pool::Pool::with_capacity(i, 0, || Vec::<u8>::new());
+            let mut checkouts = Vec::new();
+            for _ in 0..(i/3) {
+                checkouts.push(pool.checkout().unwrap());
+            }
+            b.iter(|| {
+                black_box(pool.checkout().unwrap());
+            });
+            black_box(checkouts);
+        })
+        .with_function("object_pool", |b, &&i| {
+            let pool = object_pool::Pool::new(i, || Vec::<u8>::new());
+            let mut checkouts = Vec::new();
+            for _ in 0..(i/3) {
+                checkouts.push(pool.pull().unwrap());
+            }
+            b.iter(|| {
+                black_box(pool.pull().unwrap());
+            });
+            black_box(checkouts);
+        });
+    c.bench("checkout_third", bench);
+}
+
+
+criterion_group!(benches, checkout_once, checkout_twice, checkout_last, checkout_third);
 criterion_main!(benches);
